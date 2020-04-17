16eaee9e01411d6ed44f0967d52e2071a7305476
Hongbing <cshbli@hotmail.com>
2020-04-17 11:22:25 -0400

    Add visualization for .meta

--- vis_pb_tensorboard.py
+++ vis_pb_tensorboard.py
@@ -7,20 +7,24 @@ from tensorflow.core.framework import graph_pb2
 def parse_args():
     parser = argparse.ArgumentParser(description = 'Visualize Tensorflow .pb model with Tensorboard')    
 
-    parser.add_argument('model_path', type = str, help = 'The Tensorflow model path', default = None)
+    parser.add_argument('--logdir',     type=str, default="__tb", help="The output logdir")
+    parser.add_argument('model_path',   type=str, default = None, help = 'The Tensorflow model path')
 
     return parser.parse_args()
 
 
 def main(args):
     sess = tf.Session()
-    with tf.io.gfile.GFile(args.model_path, "rb") as f:
-        graph_def = graph_pb2.GraphDef()
-        graph_def.ParseFromString(f.read())
-        tf.import_graph_def(graph_def)
+    if args.model_path.endswith('.pb'):         
+        with tf.io.gfile.GFile(args.model_path, "rb") as f:
+            graph_def = graph_pb2.GraphDef()
+            graph_def.ParseFromString(f.read())
+            tf.import_graph_def(graph_def)
+    elif args.model_path.endswith('.meta'):
+        tf.train.import_meta_graph("your-meta-graph-file.meta")
 
-    FileWriter("__tb", sess.graph)    
-    print("Model Imported. Visualize by running: tensorboard --logdir=__tb")
+    FileWriter(args.logdir, sess.graph)
+    print("Model Imported. Visualize by running: tensorboard --logdir=" + args.logdir)
 
 
 if __name__ == "__main__":
